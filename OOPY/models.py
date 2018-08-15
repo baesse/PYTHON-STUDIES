44c307556ab762b009cab3731abca81ad3f69530
Igor Baesse de Araujo <igorbaesse@timcoo.com.br>
2018-08-15 13:55:49 -0300

    create project with django

--- OOPY/models.py
+++ OOPY/models.py
@@ -1,8 +1,39 @@
 class Perfil():
-    def __init__(self,nome,telefone,empresa):
-        self.nome=nome
-        self.telefone=telefone
-        self.empresa=empresa
+    def __init__(self,name,cellphone_number,company,tipo):
+        self.name=name
+        self.cellphone_number=cellphone_number
+        self.empresa=company
+        self.__likes=0
+        self.__tipo=tipo
 
     def print_perfil(self):
-        print 'Nome:%s,  Telefone:%s,  Empresa%s ' %(self.nome,self.telefone,self.empresa)
+        print 'Nome:%s,  Telefone:%s,  Empresa%s ' %(self.name,self.cellphone_number,self.company)
+
+    def liked(self):
+        self.__likes+=1
+    
+    def get_likes(self):
+        return self.__likes
+        
+  
+   
+
+class Perfil_Vip():
+    def __init__(self,name,cellphone_number,company,tipo):
+        self.name=name
+        self.cellphone_number=cellphone_number
+        self.empresa=company
+        self.__likes=0
+        self.__tipo=tipo
+
+    def print_perfil(self):
+        print 'Nome:%s,  Telefone:%s,  Empresa%s ' %(self.name,self.cellphone_number,self.company)
+
+    def liked(self):
+        self.__likes+=1
+    
+    def get_likes(self):
+        return self.__likes
+        
+    def get_credites(self):
+        return self.__likes*10
